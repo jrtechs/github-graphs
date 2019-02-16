c9f54ad0381b31dbcde0c2b1779b97cb077d0d2e
jrtechs <jxr8142@rit.edu>
2019-02-16 14:19:54 -0500

    Moved files around to work with nodejs static routes.

---
+++ public/js/githubAPI.js
@@ -0,0 +1,44 @@
+/**
+ * Simple file which uses jQuery's ajax
+ * calls to make it easier to get data
+ * from the github api.
+ *
+ * @author Jeffery Russell 2-16-19
+ */
+
+
+const APIROOT = "https://api.github.com";
+
+const API_USER_PATH = "/users/";
+
+const API_FOLLOWING = "/following";
+
+const API_FOLLOWERS = "/followers";
+
+const API_REPOSITORIES = "/repos";
+
+const API_ORGANIZATIONS = "/orgs";
+
+
+/**
+ * Builds a query for the github rest api and
+ * allows you to get at the data using a
+ * callback which gives you a json object.
+ *
+ * @param apiPath the path on the github api ie API_FOLLOWING
+ * @param user the username to query
+ * @param successCallBack callback to complete when data is returned
+ * @param errorCallBack callback which is invoked on error
+ */
+function queryAPIByUser(apiPath, user, successCallBack, errorCallBack) {
+    const urlpath = APIROOT + API_USER_PATH + user + apiPath;
+    console.log(urlpath);
+    $.ajax({
+        type:'GET',
+        url: urlpath,
+        crossDomain: true,
+        dataType: "json",
+        success: successCallBack,
+        error:errorCallBack
+    });
+}
